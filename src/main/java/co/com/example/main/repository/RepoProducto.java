1491e7fef47174df2035d7de8742ac666daae23c
Alejandro Lopez <edwardlopez625@gmail.com>
2020-09-12 21:19:35 -0500

    modificado RepoProducto:

--- src/main/java/co/com/example/main/repository/RepoProducto.java
+++ src/main/java/co/com/example/main/repository/RepoProducto.java
@@ -2,10 +2,27 @@ package co.com.example.main.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.data.jpa.repository.Query;
+
 import co.com.example.main.domain.Producto;
 
 public interface RepoProducto extends CrudRepository<Producto, Integer>{
 
 	Producto findById(int id);
+	
+	@Query("Select p from Producto p where p.nombre like '%  ?1  %'")
+	public Iterable<Producto> buscarProductoConPalabra(String titulo);
+	
+	@Query("Select p from Producto p order by p.precio ASC")
+	public Iterable<Producto> ordenarPorPrecioAsc();
+	
+	@Query("Select p from Producto p order by p.precio DESC")
+	public Iterable<Producto> ordenarPorPrecioDesc();
+	
+	@Query("Select p from Producto p join Subcategoria s on s.id=p.subcategoria where s.nombre=' ?1 ' ")
+	public Iterable<Producto> ordenarPorSubcategoria(String nombreCategoria);
+	
+	@Query("Select p from Producto p join Proveedor pr on pr.id=p.proveedor where pr.nombre=' ?1 ' ")
+	public Iterable<Producto> ordenarPorProveedor(String nombreProveedor);
 	
 }
