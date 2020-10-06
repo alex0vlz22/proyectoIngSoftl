07ae36b6a497411aa15b9219fb76f0ebe5ae313d
Junior Alexis Llantén Vélez <junioralexislv@gmail.com>
2020-10-06 00:03:20 -0500

    Ahora cada cliente puede ver sus pedidos en la plataforma.

---
+++ src/main/java/co/com/example/main/controller/CtlPedido.java
@@ -0,0 +1,33 @@
+package co.com.example.main.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import co.com.example.main.domain.Producto;
+import co.com.example.main.domain.Usuario;
+import co.com.example.main.repository.RepoPedido;
+import co.com.example.main.repository.RepoUsuario;
+
+@Controller
+public class CtlPedido {
+
+	@Autowired
+	private RepoPedido repoPedido;
+	
+	@Autowired
+	private RepoUsuario repoUsuario;
+	
+	@GetMapping("/visualizarPedidos/{idUsuario}")
+	public String visualizarPedidos(Model model, @PathVariable("idUsuario") int idUsuario) {
+		Usuario user = this.repoUsuario.findById(idUsuario);
+		model.addAttribute("usuario", user);
+		model.addAttribute("listaPedidos", this.repoPedido.findByUsuario(PageRequest.of(0, 6), user));
+		model.addAttribute("producto", new Producto());
+		return "visualizarPedidos";
+	}
+	
+}
