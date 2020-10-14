812f51d0703ee60909588f9ce8c6dbee18765dc0
jeyson ortiz perez <jeisonortis12@gmail.com>
2020-10-14 11:55:44 +0200

    se terminaron las pruebas de integracion de repoUsuario

--- src/test/java/co/com/example/main/repository/RepoUsuarioTest.java
+++ src/test/java/co/com/example/main/repository/RepoUsuarioTest.java
@@ -3,6 +3,7 @@ package co.com.example.main.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.List;
 
@@ -23,8 +24,22 @@ public class RepoUsuarioTest {
 	@Autowired
 	private TestEntityManager entityManager;
 	
+	private Usuario getUsuario(String dni) {
+		Usuario user = new Usuario();
+		user.setDNI(dni);
+		user.setNombre("j"+dni);
+		user.setApellido("j"+dni);
+		user.setTelefono("5754567");
+		user.setCorreo("j"+dni+"@gmail.com");
+		user.setRol("Cliente");
+		user.setUrlFoto("www");
+		user.setDireccion("dffd");
+		return user;
+	}
+	
 	@Autowired
 	RepoUsuario repoUsuario;
+	
 	@Test
 	public void testListarVacio() {
 		Iterable<Usuario> lista = repoUsuario.findAll();
@@ -33,18 +48,48 @@ public class RepoUsuarioTest {
 	
 	@Test
 	public void testGuardar() {
-		Usuario user = new Usuario();
-		user.setDNI("1");
-		user.setNombre("j");
-		user.setApellido("j");
-		user.setTelefono("5754567");
-		user.setCorreo("j@gmail.com");
-		user.setRol("Cliente");
-		user.setUrlFoto("www");
-		user.setDireccion("dffd");
-		Usuario usuario = entityManager.persist(user);
+		Usuario usuario = entityManager.persist(getUsuario("1"));
 		assertNotNull(usuario);
 	}
 	
+	@Test
+	public void testBuscarId() {
+		Usuario usuario = entityManager.persist(getUsuario("1"));
+		Usuario b = repoUsuario.findById(usuario.getId());
+		assertThat(usuario).isEqualTo(b);
+	}
 	
+	@Test
+	public void TestModificar() {
+		Usuario usuario = entityManager.persist(getUsuario("1"));
+		usuario.setNombre("t1");
+		usuario.setApellido("t1");
+		Usuario update = repoUsuario.save(usuario);
+		assertThat(update).isEqualTo(usuario);
+	}
+	
+	@Test
+	public void testEliminar() {
+		Usuario usuario = entityManager.persist(getUsuario("1"));
+		repoUsuario.delete(usuario);
+		Usuario b = repoUsuario.findById(usuario.getId());
+		assertNull(b);
+	}
+	
+	@Test
+	public void testListarTodos() {
+		entityManager.persist(getUsuario("1"));
+		entityManager.persist(getUsuario("2"));
+		entityManager.persist(getUsuario("3"));
+		
+		Iterable<Usuario> lista = repoUsuario.findAll();
+		assertThat(lista).isNotEmpty();
+	}
+	
+	@Test
+	public void testBuscarDni() {
+		Usuario usuario = entityManager.persist(getUsuario("1"));
+		Usuario b = repoUsuario.findByDNI(usuario.getDNI());
+		assertThat(usuario).isEqualTo(b);
+	}
 }
