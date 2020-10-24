fc0a0b727cb9ce89f96a04b75455989ad9e3eb3a
Alejandro Lopez <edwardlopez625@gmail.com>
2020-10-24 13:49:38 -0500

    terminadas las validaciones en el registro y modificacion de las categorias

--- src/main/java/co/com/example/main/controller/CtlCategoria.java
+++ src/main/java/co/com/example/main/controller/CtlCategoria.java
@@ -100,15 +100,41 @@ public class CtlCategoria {
 	}
 
 	@PostMapping("/modificarCategoria/{id}")
-	public String editarCategoria(@PathVariable int id, Categoria categoria, Model model) {
+	public String editarCategoria(@Valid Categoria categoria, BindingResult result, @PathVariable int id, Model model) {
 		categoria.setUsuario(repoCategoria.findById(id).getUsuario());
 		categoria.setId(id);
+		if (result.hasErrors()) {
+			model.addAttribute("categoria", categoria);
+			model.addAttribute("usuario", categoria.getUsuario());
+			model.addAttribute("producto", new Producto());
+			model.addAttribute("error", " ");
+			return "editarCategoria";
+		}
+		if (existenCategorias(categoria)) {
+			model.addAttribute("categoria", categoria);
+			model.addAttribute("usuario", categoria.getUsuario());
+			model.addAttribute("producto", new Producto());
+			model.addAttribute("error", "Ya existe una categoria con el mismo nombre");
+			return "editarCategoria";
+		}
+
 		repoCategoria.save(categoria);
 		model.addAttribute("categoria", new Categoria());
 		model.addAttribute("listaCategorias", repoCategoria.findAll());
 		model.addAttribute("idVendedor", categoria.getUsuario().getId());
 		model.addAttribute("producto", new Producto());
 		return "redirect:/registroCategoria/" + categoria.getUsuario().getId();
+	}
+
+	private boolean existenCategorias(Categoria categoria) {
+		try {
+			if (repoCategoria.findByNombre(categoria.getNombre()).getId() != categoria.getId()) {
+				return true;
+			}
+
+		} catch (Exception e) {
+		}
+		return false;
 	}
 
 	@GetMapping("/eliminarCategoria/{id}")
